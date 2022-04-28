2873bbf3699384349fb47647a7e6f0d7ea9ea1e2
FedorTregubov <f.a.tregubov@yandex.ru>
2022-04-28 18:23:41 +0200

    automate deploy

--- gh-pages-deploy.sh
+++ gh-pages-deploy.sh
@@ -3,20 +3,19 @@
 # abort on errors
 set -e
 
+# lint
+npm run lint
+npm run typecheck
+
+# test
+npm run test:unit
+
 # build
+rm -rf ./dist
 npm run build
 
-# navigate into the build output directory
-cd dist
-
-# init and push gh-pages repo
-git init
-git checkout -b main
-git add -A
-git commit -m 'deploy'
-
+# init subtree and push gh-pages-branch
+git checkout gh-pages
+git add dist -f
+git commit -m 'deploy' # TODO: add release number here
 git subtree push --prefix dist origin gh-pages
-# return to the previous directory
-cd -
-
-git checkout master
