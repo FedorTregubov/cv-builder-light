95207f21dd9c90138b479f5fde99b67e457e2a3c
FedorTregubov <f.a.tregubov@yandex.ru>
2022-04-28 23:04:08 +0200

    added --force flag for deploying to gh-pages

--- gh-pages-deploy.sh
+++ gh-pages-deploy.sh
@@ -18,4 +18,4 @@ npm run build
 git checkout gh-pages
 git add dist -f
 git commit -m 'deploy' # TODO: add release number here
-git subtree push --prefix dist origin gh-pages
+git push origin `git subtree split --prefix dist master`:gh-pages --force
