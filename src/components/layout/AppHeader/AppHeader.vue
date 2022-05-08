05a8b7638073209eeaa010d4b34f3e4917f79f9f
FedorTregubov <f.a.tregubov@yandex.ru>
2022-05-08 07:09:47 +0200

    fixed logo styles

--- src/components/layout/AppHeader/AppHeader.vue
+++ src/components/layout/AppHeader/AppHeader.vue
@@ -2,7 +2,10 @@
   <header>
     <div class="container mx-auto">
       <div class="flex justify-between align-middle py-5">
-        <span class="uppercase text-sky-300">CV Builder <sup class="lowercase">Light</sup></span>
+        <span class="inline-flex items-center uppercase text-sky-300">
+          CV Builder
+          <sup class="ml-1 lowercase">Light</sup>
+        </span>
 
         <button
           class="inline-flex items-center h-8 px-5 rounded-full outline-none bg-sky-300 font-semibold text-white"
